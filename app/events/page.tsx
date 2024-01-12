a8c78aaf87cc2b2d738a53827020c2b37c367924
BarneyLoosemore1 <barney.loosemore@telegraph.co.uk>
2024-01-12 14:17:48 +0000

    Move from server filtering to client filtering + add search

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,18 +1,9 @@
 import { EventCardList } from "@/components/event-card-list";
-import { Filter } from "@/components/filter";
 import prisma from "@/lib/prisma";
+import { Suspense } from "react";
 
-const getOpts = (sort?: "asc" | "past") => {
-  const orderBy =
-    sort === "asc" ? { date: "asc" as const } : { createdAt: "desc" as const };
-  const where = {
-    date: {
-      [sort === "past" ? "lt" : "gte"]: new Date(),
-    },
-  };
-  return {
-    orderBy,
-    where,
+export default async function EventsPage() {
+  const events = await prisma.event.findMany({
     include: {
       attendees: {
         include: {
@@ -20,45 +11,11 @@ const getOpts = (sort?: "asc" | "past") => {
         },
       },
     },
-  };
-};
-
-export default async function EventsPage({
-  searchParams: { sort },
-}: {
-  searchParams: {
-    sort?: "asc" | "past";
-  };
-}) {
-  const opts = getOpts(sort);
-  const events = await prisma.event.findMany(opts);
+  });
 
   return (
-    <>
-      <search className="mb-8 mt-2 flex gap-2">
-        <Filter href="/events" active={!sort}>
-          Newly added
-        </Filter>
-        <Filter
-          href={{
-            pathname: "/events",
-            query: { sort: "asc" },
-          }}
-          active={sort === "asc"}
-        >
-          Upcoming
-        </Filter>
-        <Filter
-          href={{
-            pathname: "/events",
-            query: { sort: "past" },
-          }}
-          active={sort === "past"}
-        >
-          Past
-        </Filter>
-      </search>
+    <Suspense fallback={"Loading.."}>
       <EventCardList events={events} />
-    </>
+    </Suspense>
   );
 }
--- components/event-card-list.tsx
+++ components/event-card-list.tsx
@@ -1,6 +1,11 @@
+"use client";
 import type { Event, User } from "@prisma/client";
 import Link from "next/link";
 import { EventCard } from "./event-card";
+
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { Suspense, useMemo, useTransition } from "react";
+import { Field } from "./form/field";
 
 export type EventWithAttendees = Event & {
   attendees: {
@@ -14,16 +19,94 @@ type EventCardListProps = {
   events: EventWithAttendees[];
 };
 
-export const EventCardList = ({ events }: EventCardListProps) => (
-  <section>
-    <ul className="grid grid-cols-1 gap-y-8 sm:gap-x-12 sm:gap-y-16 md:grid-cols-3 lg:grid-cols-4">
-      {events.map((event) => (
-        <li key={event.id}>
-          <Link href={`/events/${event.id}`}>
-            <EventCard {...event} />
-          </Link>
-        </li>
-      ))}
-    </ul>
-  </section>
-);
+const filterTypes = ["all", "past", "upcoming"] as const;
+type Filter = (typeof filterTypes)[number];
+
+export const EventCardList = ({ events }: EventCardListProps) => {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+  const [_, startTransition] = useTransition();
+
+  const eventsFilter = searchParams.get("filter") as Filter | null;
+  const searchQuery = searchParams.get("search") || "";
+
+  const filteredEvents = useMemo(
+    () =>
+      events
+        .filter((event) => {
+          if (eventsFilter === "all" || !eventsFilter) return true;
+          if (eventsFilter === "past") {
+            return new Date(event.date) < new Date();
+          }
+          if (eventsFilter === "upcoming") {
+            return new Date(event.date) > new Date();
+          }
+        })
+        .filter((event) =>
+          event.title.toLowerCase().includes(searchQuery.toLowerCase()),
+        ),
+    [events, eventsFilter, searchQuery],
+  );
+
+  const handleFilterChange = (filterType: Filter) => () => {
+    const params = new URLSearchParams(searchParams);
+    params.set("filter", filterType);
+    replace(`${pathname}?${params.toString()}`);
+  };
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const params = new URLSearchParams(searchParams);
+    startTransition(() => params.set("search", e.target.value));
+    replace(`${pathname}?${params.toString()}`);
+  };
+
+  return (
+    <>
+      <search>
+        <ul className="mb-2 flex gap-4">
+          {filterTypes.map((filterType) => (
+            <li key={filterType}>
+              <button
+                onClick={handleFilterChange(filterType)}
+                className={`border-b-2 pb-1 text-sm text-slate-700 hover:opacity-90 sm:text-lg ${
+                  eventsFilter === filterType
+                    ? "border-b-slate-700"
+                    : "border-b-transparent"
+                }`}
+              >
+                {filterType}
+              </button>
+            </li>
+          ))}
+        </ul>
+
+        <form
+          className="mb-4 flex flex-col sm:max-w-64"
+          onSubmit={(e) => e.preventDefault()}
+        >
+          <Field
+            label="Search"
+            name="search"
+            type="search"
+            placeholder="e.g. Crazy Event"
+            onChange={handleSearchChange}
+          />
+        </form>
+      </search>
+      <Suspense key={searchQuery} fallback={"Searching.."}>
+        <section>
+          <ul className="grid grid-cols-1 gap-y-8 sm:gap-x-12 sm:gap-y-16 md:grid-cols-3 lg:grid-cols-4">
+            {filteredEvents.map((event) => (
+              <li key={event.id}>
+                <Link href={`/events/${event.id}`}>
+                  <EventCard {...event} />
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </section>
+      </Suspense>
+    </>
+  );
+};
